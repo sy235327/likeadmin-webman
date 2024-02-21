6e07f58c511fbea6bccc5cbd1ea85480361fea44
suyi <suyibk@qq.com>
2024-02-21 17:23:47 +0800

    1

--- admin/src/api/user.ts
+++ admin/src/api/user.ts
@@ -13,10 +13,10 @@ export function logout() {
 
 // 用户信息
 export function getUserInfo() {
-    return request.get({ url: '/auth/admin/mySelf' })
+    return request.get({ url: '/auth/adminapi/mySelf' })
 }
 
 // 编辑管理员信息
 export function setUserInfo(params: any) {
-    return request.post({ url: '/auth/admin/editSelf', params })
+    return request.post({ url: '/auth/adminapi/editSelf', params })
 }

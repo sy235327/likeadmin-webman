59bf6492a0de4273f4b3445b0d639b9cca4e989e
suyi <suyibk@qq.com>
2024-03-05 15:48:47 +0800

    队列推送

--- server/app/queue/redis/MySendMail.php
+++ server/app/queue/redis/MySendMail.php
@@ -18,5 +18,24 @@ class MySendMail implements Consumer
         // 无需反序列化
         var_export($data);
     }
-            
+    // 消费失败回调
+    /*
+    $package = [
+        'id' => 1357277951, // 消息ID
+        'time' => 1709170510, // 消息时间
+        'delay' => 0, // 延迟时间
+        'attempts' => 2, // 消费次数
+        'queue' => 'send-mail', // 队列名
+        'data' => ['to' => 'tom@gmail.com', 'content' => 'hello'], // 消息内容
+        'max_attempts' => 5, // 最大重试次数
+        'error' => '错误信息' // 错误信息
+    ]
+    */
+    public function onConsumeFailure(\Throwable $e, $package)
+    {
+        echo "consume failure\n";
+        echo $e->getMessage() . "\n";
+        // 无需反序列化
+        var_export($package);
+    }
 }
---
+++ server/app/queue/send/SendQueue.php
@@ -0,0 +1,46 @@
+<?php
+namespace app\queue\send;
+
+use Webman\RedisQueue\Client;
+use Webman\RedisQueue\Redis;
+
+/**
+ * 推送消息到队列
+ */
+class SendQueue
+{
+    /**
+     * 异步插入队列
+     * @param $queue_name string 队列名
+     * @param $data 数据，可以直接传数组，无需序列化 例如：['to' => 'tom@gmail.com', 'content' => 'hello']
+     * @param $delay int 0 是否投递延迟消息 延迟多少秒
+     */
+    public function queueAsync(string $queue_name,$data,int $delay = 0)
+    {
+        if ($delay === null){
+            // 投递消息
+            Client::send($queue_name, $data);
+        }else{
+            // 投递延迟消息，消息会在60秒后处理
+            Client::send($queue_name, $data, $delay);
+        }
+    }
+    /**
+     * 同步插入队列
+     * @param $queue_name string 队列名
+     * @param $data 数据，可以直接传数组，无需序列化 例如：['to' => 'tom@gmail.com', 'content' => 'hello']
+     * @param $delay int 0 是否投递延迟消息 延迟多少秒
+     * @return boolean 是否成功
+     */
+    public function queueSync(string $queue_name,$data,int $delay = 0)
+    {
+        if ($delay === null){
+            // 投递消息
+            return Redis::send($queue_name, $data);
+        }else{
+            // 投递延迟消息，消息会在60秒后处理
+            return Redis::send($queue_name, $data, $delay);
+        }
+    }
+
+}

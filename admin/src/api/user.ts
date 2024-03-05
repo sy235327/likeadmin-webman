8e8e54d389089843cd698e68b0e821ea6bef5791
suyi <suyibk@qq.com>
2024-03-05 16:08:01 +0800

    bug

--- admin/src/api/user.ts
+++ admin/src/api/user.ts
@@ -13,10 +13,10 @@ export function logout() {
 
 // 用户信息
 export function getUserInfo() {
-    return request.get({ url: "/auth/adminapi/mySelf" })
+    return request.get({ url: "/auth/admin/mySelf" })
 }
 
 // 编辑管理员信息
 export function setUserInfo(params: any) {
-    return request.post({ url: "/auth/adminapi/editSelf", params })
+    return request.post({ url: "/auth/admin/editSelf", params })
 }

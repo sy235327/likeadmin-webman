c7f5571e7260d80a0ee71f57be7d71a92f5fff28
suyi <suyibk@qq.com>
2024-03-08 13:19:32 +0800

    1

--- server/app/adminapi/lists/article/ArticleCateLists.php
+++ server/app/adminapi/lists/article/ArticleCateLists.php
@@ -1,16 +1,33 @@
 <?php
-
+// +----------------------------------------------------------------------
+// | likeadmin快速开发前后端分离管理后台（PHP版）
+// +----------------------------------------------------------------------
+// | 欢迎阅读学习系统程序代码，建议反馈是我们前进的动力
+// | 开源版本可自由商用，可去除界面版权logo
+// | gitee下载：https://gitee.com/likeshop_gitee/likeadmin
+// | github下载：https://github.com/likeshop-github/likeadmin
+// | 访问官网：https://www.likeadmin.cn
+// | likeadmin团队 版权所有 拥有最终解释权
+// +----------------------------------------------------------------------
+// | author: likeadminTeam
+// +----------------------------------------------------------------------
 
 namespace app\adminapi\lists\article;
-
 
 use app\adminapi\lists\BaseAdminDataLists;
 use app\common\lists\ListsSearchInterface;
 use app\common\lists\ListsSortInterface;
-use app\common\model\article\Article;
+use app\common\model\article\ArticleCate;
 
-class ArticleLists extends BaseAdminDataLists implements ListsSearchInterface, ListsSortInterface
+/**
+ * 资讯分类列表
+ * Class ArticleCateLists
+ * @package app\adminapi\lists\article
+ */
+class ArticleCateLists extends BaseAdminDataLists implements ListsSearchInterface, ListsSortInterface
 {
+
+
     /**
      * @notes  设置搜索条件
      * @return array
@@ -19,10 +36,7 @@ class ArticleLists extends BaseAdminDataLists implements ListsSearchInterface, L
      */
     public function setSearch(): array
     {
-        return [
-            '%like%' => ['title'],
-            '=' => ['cid', 'is_show']
-        ];
+        return [];
     }
 
     /**
@@ -44,7 +58,7 @@ class ArticleLists extends BaseAdminDataLists implements ListsSearchInterface, L
      */
     public function setDefaultOrder(): array
     {
-        return ['sort' => 'desc', 'id' => 'desc'];
+        return ['sort' => 'desc','id' => 'desc'];
     }
 
     /**
@@ -55,14 +69,15 @@ class ArticleLists extends BaseAdminDataLists implements ListsSearchInterface, L
      */
     public function lists(): array
     {
-        $ArticleLists = Article::where($this->searchWhere)
-            ->append(['cate_name', 'click'])
+        $ArticleCateLists = ArticleCate::where($this->searchWhere)
+            ->append(['is_show_desc'])
             ->limit($this->limitOffset, $this->limitLength)
             ->order($this->sortOrder)
+            ->append(['article_count'])
             ->select()
             ->toArray();
 
-        return $ArticleLists;
+        return $ArticleCateLists;
     }
 
     /**
@@ -73,7 +88,7 @@ class ArticleLists extends BaseAdminDataLists implements ListsSearchInterface, L
      */
     public function count(): int
     {
-        return Article::where($this->searchWhere)->count();
+        return ArticleCate::where($this->searchWhere)->count();
     }
 
     public function extend()

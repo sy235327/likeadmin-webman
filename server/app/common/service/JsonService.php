4fb53306da9cea61da217d16879928afb1ba8a9f
suyi <suyibk@qq.com>
2024-11-16 14:18:57 +0800

    注解

--- server/app/common/service/JsonService.php
+++ server/app/common/service/JsonService.php
@@ -15,56 +15,60 @@ class JsonService
 {
     /**
      * @notes 接口操作成功，返回信息
-     * @param string $msg
-     * @param array $data
-     * @param int $code
-     * @param int $show
+     * @param string $msg 信息
+     * @param array $data 数据
+     * @param int $code 状态码
+     * @param int $show 是否显示
+     * @param int $httpStatus http状态码
+     * @param int $options 可选 JSON_UNESCAPED_UNICODE json 格式化
      * @author 乔峰
      * @date 2021/12/24 18:28
      */
-    public static function success(string $msg = 'success', array $data = [], int $code = 1, int $show = 1)
+    public static function success(string $msg = 'success', array $data = [], int $code = 1, int $show = 1, int $httpStatus = 200,...$options): Response
     {
-        return self::result($code, $show, $msg, $data);
+        return self::result($code, $show, $msg, $data, $httpStatus,...$options);
     }
 
 
     /**
      * @notes 接口操作失败，返回信息
-     * @param string $msg
-     * @param array $data
-     * @param int $code
-     * @param int $show
+     * @param string $msg 信息
+     * @param array $data 数据
+     * @param int $code 状态码
+     * @param int $show 是否显示
+     * @param int $httpStatus http状态码
+     * @param int $options 可选 JSON_UNESCAPED_UNICODE json 格式化
      * @author 乔峰
      * @date 2021/12/24 18:28
      */
-    public static function fail(string $msg = 'fail', array $data = [], int $code = 0, int $show = 1)
+    public static function fail(string $msg = 'fail', array $data = [], int $code = 0, int $show = 1, int $httpStatus = 200,...$options): Response
     {
-        return self::result($code, $show, $msg, $data);
+        return self::result($code, $show, $msg, $data, $httpStatus,...$options);
     }
 
     /**
      * @notes 接口返回数据
-     * @param $data
+     * @param array $data 数据
      * @author 乔峰
      * @date 2021/12/24 18:29
      */
-    public static function data($data)
+    public static function data(array $data): Response
     {
         return self::success('', $data, 1, 0);
     }
 
     /**
      * @notes 接口返回信息
-     * @param int $code
-     * @param int $show
-     * @param string $msg
-     * @param array $data
-     * @param int $httpStatus
-     * @param int $options json 格式化
+     * @param int $code 状态码
+     * @param int $show 是否显示
+     * @param string $msg 信息
+     * @param array $data 数据
+     * @param int $httpStatus http状态码
+     * @param int $options 可选 JSON_UNESCAPED_UNICODE json 格式化
      * @author 乔峰
      * @date 2021/12/24 18:29
      */
-    private static function result(int $code, int $show, string $msg = 'OK', array $data = [], int $httpStatus = 200,...$options)
+    private static function result(int $code, int $show, string $msg = 'OK', array $data = [], int $httpStatus = 200,...$options): Response
     {
         $result = compact('code', 'show', 'msg', 'data');
         return json($result, $httpStatus,...$options);
@@ -72,10 +76,10 @@ class JsonService
 
     /**
      * @notes 抛出异常json
-     * @param string $msg
-     * @param array $data
-     * @param int $code
-     * @param int $show
+     * @param string $msg 信息
+     * @param array $data 数据
+     * @param int $code 状态码
+     * @param int $show 是否显示
      * @author 乔峰
      * @date 2021/12/24 18:29
      */
@@ -87,11 +91,11 @@ class JsonService
 
     /**
      * @notes 数据列表
-     * @param \app\common\lists\BaseDataLists $lists
+     * @param BaseDataLists $lists 数据列表
      * @author 令狐冲
      * @date 2021/7/28 11:15
      */
-    public static function dataLists(BaseDataLists $lists)
+    public static function dataLists(BaseDataLists $lists): Response
     {
         //获取导出信息
         if ($lists->export == ExportEnum::INFO && $lists instanceof ListsExcelInterface) {

6f09a4b9bf4804f9f7366ea605d2db5a34a5aaf1
suyi <suyibk@qq.com>
2025-02-08 17:47:29 +0800

    配置错误修复

--- server/config/plugin/webman/redis-queue/redis.php
+++ server/config/plugin/webman/redis-queue/redis.php
@@ -3,7 +3,7 @@ return [
     'default' => [
         'host' => 'redis://'.getenv('REDIS_QUEUE_HOST','127.0.0.1').':'.getenv('REDIS_QUEUE_PORT','6379'),
         'options' => [
-            'auth' => getenv('REDIS_QUEUE_PASSWORD',null),       // 密码，字符串类型，可选参数
+            'auth' => getenv('REDIS_QUEUE_PASSWORD',''),       // 密码，字符串类型，可选参数
             'db' => getenv('REDIS_QUEUE_DB',0),            // 数据库
             'prefix' => getenv('REDIS_QUEUE_PREFIX',''),       // key 前缀
             'max_attempts'  => 5, // 消费失败后，重试次数
